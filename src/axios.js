b4d7e96c468ae93913da884f340fc49a5e26c023
Vishnu CP <vishnucpmcd@gmail.com>
2024-01-26 19:54:39 +0530

    server url changed in axios and env

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:process.env.REACT_APP_SERVER_URL
+    baseURL:"https://tasteofpalakkad-backend.onrender.com"
 })
 
 export default instance;
